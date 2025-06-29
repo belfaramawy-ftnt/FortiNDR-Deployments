adcea000477c518ce9b526b62b05b31976df82c1
belfaramawy-ftnt <belfaramawy@fortinet.com>
2025-06-29 18:02:37 +0300

    force the outputs to appear in this specific order

--- oci/7.6.2/standalone/outputs.tf
+++ oci/7.6.2/standalone/outputs.tf
@@ -1,36 +1,37 @@
-output "deployment_notice" {
+output "01_deployment_notice" {
   description = "Important deployment information"
   value       = "⚠️  WAIT 2-3 minutes after deployment completes, then access FortiNDR UI at: https://${oci_core_instance.fndr_sensor.public_ip}"
 }
 
-output "vm_name" {
+output "02_vm_name" {
   description = "Name of the VM"
   value       = oci_core_instance.fndr_sensor.display_name
 }
 
-output "public_ip" {
-  description = "Public IP address of the VM"
-  value       = oci_core_instance.fndr_sensor.public_ip
-}
-
-output "private_ip" {
-  description = "Private IP address of the VM"
-  value       = oci_core_instance.fndr_sensor.private_ip
-}
-
-output "sniffer_private_ip" {
-  description = "Private IP address of the sniffer interface"
-  value       = data.oci_core_vnic.sniffer_vnic.private_ip_address
-}
-
-output "username" {
+output "03_username" {
   description = "Default username"
   value       = "Admin"
 }
 
-output "password" {
+output "04_password" {
   description = "Password (VM OCID)"
   value       = oci_core_instance.fndr_sensor.id
+  sensitive   = true
+}
+
+output "05_mgmt_public_ip" {
+  description = "Public IP address of the VM"
+  value       = oci_core_instance.fndr_sensor.public_ip
+}
+
+output "06_mgmt_private_ip" {
+  description = "Private IP address of the VM"
+  value       = oci_core_instance.fndr_sensor.private_ip
+}
+
+output "07_sniffer_private_ip" {
+  description = "Private IP address of the sniffer interface"
+  value       = data.oci_core_vnic.sniffer_vnic.private_ip_address
 }
 
 # Data source to get sniffer VNIC details
